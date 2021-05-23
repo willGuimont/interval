ed9378b3e108f05ffaa0290d7d2181e97fa3bca1
William Guimont-Martin <wil9876543210@hotmail.com>
2021-05-22 22:27:18 -0400

    project creation

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject interval "0.1.0-SNAPSHOT"
+  :description "interval work"
+  :url "https://github.com/willGuimont/interval"
+  :license {:name "MIT"
+            :url  "https://choosealicense.com/licenses/mit/"}
+  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :repl-options {:init-ns interval.core})
